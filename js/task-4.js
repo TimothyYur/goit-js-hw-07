5505977ec5733862e2881e4737a845300b98e4cb
TimothyY <timjurev@gmail.com>
2026-01-25 22:16:12 +0200

    task3-4-5 complete

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    if (email === "" || password === "") {alert('All form fields must be filled in');}
+    else{
+        const userInfo = {};
+        userInfo['email'] = email.trim();
+        userInfo['password'] = password.trim();
+        console.log(userInfo)
+    }
+    form.reset();
+});
